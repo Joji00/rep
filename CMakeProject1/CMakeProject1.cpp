d6d0bb65a88210bd1fe23063c9c3c82b9026e08f
Fakher <Fakher@DESKTOP-EK10SSG>
2022-06-15 15:47:18 +0200

    file 1

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -9,9 +9,7 @@ int main()
 {
 	cout << "LOOOOOMMMMIIIII CMake." << endl;
 	cout << "LOOOOOMMMMIIIII CMake." << endl;
-	cout << "LOOOOOMMMMIIIII CMake." << endl;
-	cout << "LOOOOOMMMMIIIII CMake." << endl;
-	cout << "LOOOOOMMMMIIIII CMake." << endl;
+	cout << "love me CMake." << endl;
 	cout << "LOOOOOMMMMIIIII CMake." << endl;
 	return 0;
 }
